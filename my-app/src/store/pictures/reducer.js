d2f9540ddbad26424af1159848eba378b87c81f6
Viktar Hlushchenia <slayer110@yandex.ru>
2019-12-29 15:38:23 +0300

    refactoring

--- my-app/src/store/pictures/reducer.js
+++ my-app/src/store/pictures/reducer.js
@@ -2,13 +2,14 @@ import {PICTURE_DATA_SUCCESS} from "./action";
 import {ADD_FAVOURITE} from "./action";
 import {DELETE_FAVOURITE} from "./action";
 
-let favouriteArr = JSON.parse(localStorage.getItem('arrFavour'));
-let listArr=JSON.parse(sessionStorage.getItem('resultsSearch'));
+let favouriteArr = JSON.parse(localStorage.getItem('arrFavour'))||[];
+let listArr = JSON.parse(sessionStorage.getItem('resultsSearch'))||[];
+let range=JSON.parse(sessionStorage.getItem('rangeNumber'))||1;
 
 const defaultState = {
-  listPictures: listArr||[],
-  countForMore: 1,
-  arrFavourite: favouriteArr||[]
+  listPictures: listArr,
+  countForMore: range,
+  arrFavourite: favouriteArr
 };
 const dataPicturesReducer = (state = defaultState, action) => {
   let auxArrFavour = state.arrFavourite.map((elem) => elem.lister_url);
@@ -19,11 +20,11 @@ const dataPicturesReducer = (state = defaultState, action) => {
   let arrFavour = [...state.arrFavourite].map((elem) => {
     return {...elem}
   });
+  let star;
   switch (action.type) {
     case PICTURE_DATA_SUCCESS:
       let {list, range} = action.payload;
       let arr;
-      let star;
       list = list.map((elem) => {
         if (auxArrFavour.indexOf(elem.lister_url) >= 0) {
           star = true;
@@ -37,6 +38,7 @@ const dataPicturesReducer = (state = defaultState, action) => {
         arr = list;
       }
       sessionStorage.setItem('resultsSearch', JSON.stringify(arr));
+      sessionStorage.setItem('rangeNumber', JSON.stringify(range));
       return {...state, countForMore: range, listPictures: arr};
     case ADD_FAVOURITE:
       if (auxArrFavour.indexOf(action.payload) >= 0) {
@@ -46,16 +48,26 @@ const dataPicturesReducer = (state = defaultState, action) => {
         arrList[arrListerUrl.indexOf(action.payload)].star = true;
         arrFavour.push(state.listPictures[arrListerUrl.indexOf(action.payload)]);
       }
-      localStorage.setItem('arrFavour',JSON.stringify(arrFavour));
+      localStorage.setItem('arrFavour', JSON.stringify(arrFavour));
       return {...state, listPictures: arrList, arrFavourite: arrFavour};
     case DELETE_FAVOURITE:
       arrFavour.splice(auxArrFavour.indexOf(action.payload), 1);
       if (arrList[arrListerUrl.indexOf(action.payload)]) {
         arrList[arrListerUrl.indexOf(action.payload)].star = false
       }
-      localStorage.setItem('arrFavour',JSON.stringify(arrFavour));
+      localStorage.setItem('arrFavour', JSON.stringify(arrFavour));
       return {...state, listPictures: arrList, arrFavourite: arrFavour};
+    default:
+      arrList = arrList.map((elem) => {
+        if (auxArrFavour.indexOf(elem.lister_url) >= 0) {
+          star = true;
+        } else {
+          star = false;
+        }
+        return {...elem, star: star}
+      });
+      return {...state, listPictures: arrList}
   }
-  return state
+
 };
 export default dataPicturesReducer;
