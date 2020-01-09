3fadbda72f55395d6031d5371804fde53838df4b
Viktar Hlushchenia <slayer110@yandex.ru>
2020-01-09 20:15:12 +0300

    format text

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,13 +3,14 @@ import '../App.css';
 
 class Footer extends React.Component {
 
-  nextPictures=()=>{
-    this.props.picturesRange(this.props.rangeNumber+1,this.props.searchInput)
+  nextPictures = () => {
+    this.props.picturesRange(this.props.rangeNumber + 1, this.props.searchInput)
   };
+
   render() {
     return (
       <div className="footer">
-        {this.props.pictures.length?<span onClick={this.nextPictures}>Further</span>:''}
+        {this.props.pictures.length ? <span onClick={this.nextPictures}>Further</span> : ''}
       </div>
     )
   }
