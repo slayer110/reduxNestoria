ba8f7225f194704f82c8cdd3ed3e8758ff7a4382
Viktar Hlushchenia <slayer110@yandex.ru>
2020-01-09 20:07:04 +0300

    creating action "actionFavour" in Favourite

--- my-app/src/components/Favourite.js
+++ my-app/src/components/Favourite.js
@@ -2,19 +2,22 @@ import React from 'react';
 import FavouritePicture from './favoritePicture';
 
 class Favourite extends React.Component {
-
-  render() {
-    let arrFavour = this.props.arrFavourite.map((elem, index) => {
+  actionFavour() {
+    let arrFavour;
+    return arrFavour = this.props.arrFavourite.map((elem, index) => {
       return <FavouritePicture key={index} image={elem.img_url} listerUrl={elem.lister_url}
                                deleteFavourite={this.props.delete}/>
     });
+  }
+
+  render() {
     return (
       <div>
         <div>
           <h1>Favourites</h1>
           {/*<div className="close" onClick={this.onVisible}><img src={close}/></div>*/}
           <div id="boxForFavourite">
-            {arrFavour}
+            {this.actionFavour()}
           </div>
         </div>
       </div>)
